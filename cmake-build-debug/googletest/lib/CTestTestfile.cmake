8aef6a53c04ae2ae6bc6ea8b7eefd4665d4d1a16
averyjacobson <77401156+averyjacobson@users.noreply.github.com>
2022-12-31 16:38:52 -0700

    Add files via upload

---
+++ cmake-build-debug/googletest/lib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/c/Users/puddi/OneDrive/Documents/Semester 5/CSCI_366/csci-366-fall2022-private/c/lmsm/googletest/lib
+# Build directory: /cygdrive/c/Users/puddi/OneDrive/Documents/Semester 5/CSCI_366/csci-366-fall2022-private/c/lmsm/cmake-build-debug/googletest/lib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
---
+++ cmake-build-debug/googletest/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /cygdrive/c/Users/puddi/OneDrive/Documents/Semester 5/CSCI_366/csci-366-fall2022-private/c/lmsm/googletest/lib/googletest
+# Build directory: /cygdrive/c/Users/puddi/OneDrive/Documents/Semester 5/CSCI_366/csci-366-fall2022-private/c/lmsm/cmake-build-debug/googletest/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
